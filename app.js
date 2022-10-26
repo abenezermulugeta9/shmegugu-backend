e19ebd71263e39d45847ddab6ce913ffb482ef05
AbenezerM <abenezermulugeta41@gmail.com>
2022-10-26 15:12:34 -0500

    Edit the routing config

--- app.js
+++ app.js
@@ -12,11 +12,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use('/api', router);
-app.use('/api', projectRouter )
-app.use('*', (req, res, next) => {
-    res.status(404);
-    next(new Error(`Router not found.`)); 
-})
+// app.use('*', (req, res, next) => {
+//     res.status(404);
+//     next(new Error(`Router not found.`)); 
+// })
 
 app.use((err, req, res, next) => {
     res.json({ err: err.message })
