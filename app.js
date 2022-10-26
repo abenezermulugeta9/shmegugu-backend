b0a7dd76bfea816b59fb210c3b441c4a75031e1f
AbenezerM <abenezermulugeta41@gmail.com>
2022-10-26 15:15:54 -0500

    Modify the config

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use('/api', router);
-// app.use('*', (req, res, next) => {
-//     res.status(404);
-//     next(new Error(`Router not found.`)); 
-// })
+app.use('*', (req, res, next) => {
+    res.status(404);
+    next(new Error(`Router not found.`)); 
+})
 
 app.use((err, req, res, next) => {
     res.json({ err: err.message })
