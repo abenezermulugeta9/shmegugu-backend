618404b2d70c037751ad2933dfecfe4b24fa5ad4
AbenezerM <abenezermulugeta41@gmail.com>
2022-10-26 22:06:34 -0500

    Remove /api from routes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/api', router);
+app.use('/', router);
 app.use('*', (req, res, next) => {
     res.status(404);
     next(new Error(`Router not found.`)); 
