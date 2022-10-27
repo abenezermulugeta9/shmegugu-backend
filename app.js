733112a7a79a26bf347e30ccaf425b469d71a68a
AbenezerM <abenezermulugeta41@gmail.com>
2022-10-26 21:46:06 -0500

    Revert back the /api route

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/', router);
+app.use('/api', router);
 app.use('*', (req, res, next) => {
     res.status(404);
-    next(new Error(`Route not found.`)); 
+    next(new Error(`Router not found.`)); 
 })
 
 app.use((err, req, res, next) => {
